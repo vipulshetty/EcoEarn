5fb0bc3abf751bae2cab8ebafc663c763f1db91f
Vipul <vipulshetty918@gmail.com>
2024-10-24 23:08:50 +0530

    Your commit message

--- Backend/test_load_model.py
+++
@@ -1,7 +0,0 @@
-from keras.models import load_model
-
-try:
-    model = load_model("model/plastic_waste_classifier.h5")
-    print("Model loaded successfully!")
-except Exception as e:
-    print("Error loading model:", str(e))
